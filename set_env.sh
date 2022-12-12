f033840ba925ebbcd6f3369cae9982758ed56b87
ali <ali@faster.myguest.virtualbox.org>
2022-12-12 15:42:54 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=ali
+export POSTGRES_PASSWORD=pass123456
+export POSTGRES_HOST=alifawzi.cuc0bupdujnd.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::80778727067proj3alifawzi
+export AWS_REGION=us-east-2b
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8080
